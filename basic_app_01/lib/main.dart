74f6431b35d35ca048fa61b113e75ed740fba73f
Abrahames <abtaham0212@gmail.com>
2024-07-11 01:20:27 -0600

    proyectos de flutter

---
+++ basic_app_01/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text("Uso de imagen", style:
+            TextStyle(fontSize: 10) ,),
+          )),
+        body: Center(
+          child: Image.asset('assets/images/LA JEFECLICKA.png'),
+        ),
+      ),
+    );
+  }
+}
